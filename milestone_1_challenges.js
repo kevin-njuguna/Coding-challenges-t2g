fd3ce4f03b791d87e4653657ec50ccd78d588a92
kevin-njuguna <kevy.kamah@gmail.com>
2025-05-21 18:35:33 +0300

    coding challenge 7: BMI Calculator

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -33,9 +33,9 @@ console.log(isNegative(55));
 //challenge 5 : Can I Drive
 function canDrive(name, age) {
   if (age >= 18) {
-    return `${name} is old enough to drive.`
+    return `${name} is old enough to drive.`;
   } else {
-    return `${name} is not old enough to drive yet.`
+    return `${name} is not old enough to drive yet.`;
   }
 }
 //console.log(canDrive("Jane", 22))
@@ -43,9 +43,31 @@ function canDrive(name, age) {
 
 //challenge 6 : Largest Number
 function findLargest(a, b, c) {
-  return Math.max(a, b, c)
+  return Math.max(a, b, c);
 }
 
-console.log(findLargest(5, 9, 3))
-console.log(findLargest(10, 10, 10))
-console.log(findLargest(-1, -5, -3))
+//console.log(findLargest(5, 9, 3));
+console.log(findLargest(10, 10, 10));
+console.log(findLargest(-1, -5, -3));
+
+//challenge 7 : BMI Calculator
+function calculateBMI(weight, height) {
+  const bmiRaw = weight / (height * height);
+  const bmi = Math.round(bmiRaw * 10) / 10; 
+  let category = "";
+
+  if (bmi < 18.5) {
+    category = "Underweight";
+  } else if (bmi < 25) {
+    category = "Normal weight";
+  } else if (bmi < 30) {
+    category = "Overweight";
+  } else {
+    category = "Obese";
+  }
+
+  return "Your BMI is " + bmi + " - " + category;
+}
+
+console.log(calculateBMI(68, 1.75));
+console.log(calculateBMI(85, 1.8));
