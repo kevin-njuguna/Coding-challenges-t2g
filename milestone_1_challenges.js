2d4c64fbb64508659f7dc59806bc25f6e24eed06
kevin-njuguna <kevy.kamah@gmail.com>
2025-05-21 18:05:25 +0300

    Completed challenge 2: convert minutes into seconds

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -3,6 +3,13 @@ function addition(num1, num2) {
   return num1 + num2;
 }
 
-console.log(addition(3, 5))
-console.log(addition(-6, 9))
+//console.log(addition(3, 5))
+//console.log(addition(-6, 9))
 
+//challenge 2 : Convert Minutes into Seconds
+function convert(min) {
+  return min * 60;
+}
+
+console.log(convert(5));
+console.log(convert(2));
