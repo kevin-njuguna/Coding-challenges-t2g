e20bccddf94d7bcaedfd40db301ee744290d1465
kevin-njuguna <kevy.kamah@gmail.com>
2025-05-23 15:07:03 +0300

    Completed challenge 2: Find Maximum Value

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -9,4 +9,18 @@ function sumOfPositives(numbers) {
 }
 
 sumOfPositives([1, -3, 5, -2, 9, -8]);
-console.log(sumOfPositives([1, -3, 5, -2, 9, -8]));
+//console.log(sumOfPositives([1, -3, 5, -2, 9, -8]));
+
+// Challenge 2: Find Maximum Value
+function findMax(numbers) {
+  let max = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+  return max;
+}
+
+findMax([3, 7, 2, 9, 5]);
+console.log(findMax([3, 7, 2, 9, 5]));
