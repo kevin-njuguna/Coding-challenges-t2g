8fb264fb0c38af50d6d780d0c449a741fddbfe70
kevin-njuguna <kevy.kamah@gmail.com>
2025-05-23 15:19:37 +0300

    Completed coding challenge 3: Find Election Winner

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -23,4 +23,24 @@ function findMax(numbers) {
 }
 
 findMax([3, 7, 2, 9, 5]);
-console.log(findMax([3, 7, 2, 9, 5]));
+//console.log(findMax([3, 7, 2, 9, 5]));
+
+// Challenge 3: Election Winner
+function findWinner(candidates) {
+  let winner = candidates[0];
+  for (let i = 1; i < candidates.length; i++) {
+    if (candidates[i].votes > winner.votes) {
+      winner = candidates[i];
+    }
+  }
+  return winner;
+}
+
+const candidates = [
+  { name: "Alice", votes: 50 },
+  { name: "Bob", votes: 75 },
+  { name: "Charlie", votes: 65 },
+];
+
+findWinner(candidates);
+console.log(findWinner(candidates));
